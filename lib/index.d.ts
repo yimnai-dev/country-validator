26394b363884a5a79d6a169df3e24a8b88c1dc6f
yimnai-dev <yimnai.nerus@gmail.com>
2023-09-10 22:02:18 +0100

    feat: Published

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,15 @@
-import { Continent, Country } from "./utils/types";
-export declare const verifyCountryExistence: (country: string) => boolean;
-export declare const getCountryCode: (countryObj: Partial<Pick<Country, 'name' | 'dial_code'>>) => {};
-export declare const getCountryDialCode: (countryObj: Partial<Pick<Country, 'code' | 'name'>>) => {};
-export declare const getCountryName: (countryObj: Partial<Pick<Country, 'code' | 'dial_code'>>) => {};
-export declare const getCountryInfo: (countryObj: Partial<Omit<Country, 'continent'>>) => {};
-export declare const verifyIfCountryInContinent: (countryObj: Partial<Pick<Country, 'name' | 'dial_code' | 'code'>>, continent: Continent) => {};
-export declare const getContinentOfCountry: (countryObj: Partial<Pick<Country, 'name' | 'dial_code' | 'code'>>) => {};
-export declare const getCountriesOfContinent: (continent: Continent, limit?: number) => {};
-export declare const getAllCountries: (limit?: number) => {};
+import { Continent, Country, CountryName, CountryCode, CountryDialCode, NameOrDialCode, NameOrCountryCode, CodeOrDialCode, NameOrCodeOrDialCode, NATION_STATES } from "./utils/types";
+export declare const verifyCountryExistence: (country: CountryName) => boolean;
+export declare const getCountryCode: (countryObj: NameOrDialCode) => CountryCode | undefined;
+export declare const getCountryDialCode: (countryObj: NameOrCountryCode) => CountryDialCode | undefined;
+export declare const getCountryName: (countryObj: CodeOrDialCode) => CountryName | undefined;
+export declare const getCountryInfo: (countryObj: NameOrCodeOrDialCode) => Country | undefined;
+export declare const verifyIfCountryInContinent: (countryObj: NameOrCodeOrDialCode, continent: Continent) => boolean;
+export declare const getContinentOfCountry: (countryObj: NameOrCodeOrDialCode) => Continent | undefined;
+export declare const getCountriesOfContinent: (continent: Continent, limit?: number) => Country[];
+export declare const getAllCountries: (limit?: number) => Country[];
 export declare const sortCountries: (sortObj?: {
     data?: Country[];
     limit?: number;
-}) => {};
+}) => Country[];
+export declare const getCountryStates: (countryObj: NameOrCodeOrDialCode) => NATION_STATES | undefined;
