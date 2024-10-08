9227cb8f8458134b4870f95a59cb09628a55cf40
yimnai-dev <yimnai-dev>
2024-10-08 16:00:01 +0100

    more stuff

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    "^.+\\.tsx?$": "ts-jest", // Transform TypeScript files using ts-jest
+    "^.+\\.js$": "babel-jest" // Transform ESM JavaScript files using babel-jest
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx", ".js"], // Treat .js files as ESM
+  transformIgnorePatterns: ["/node_modules/"], // Ignore transformation for node_modules unless necessary
 };
